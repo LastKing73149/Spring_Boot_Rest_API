b56f24bb9032487ec5260a45974c6dc4ca26c31e
Vladislav Kim <vladuskim9359@gmail.com>
2026-05-21 14:36:42 +0900

    Changed Role and user.html

--- src/main/java/com/project/work/model/Role.java
+++ src/main/java/com/project/work/model/Role.java
@@ -45,10 +45,15 @@ public class Role implements GrantedAuthority {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Role role = (Role) o;
-        return Objects.equals(id, role.id);
+        if(id != null && role.id != null) {
+            return id.equals(role.id);
+        }
+        return name != null ? name.equals(role.name) : role.name == null;
     }
     @Override
     public int hashCode(){
-        return Objects.hash(id);
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 }
